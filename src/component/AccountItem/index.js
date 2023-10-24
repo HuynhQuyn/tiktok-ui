4700a4c07f991d3fcf43c3fdb694181c644d930d
HuynhQuyn <huynhquyn0811@example.com>
2023-10-24 20:46:21 +0700

    ap dung PropTypes

--- src/component/AccountItem/index.js
+++ src/component/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import Image from '~/component/Image';
@@ -21,5 +22,7 @@ function AccountItem({ data }) {
         </Link>
     );
 }
-
+AccountItem.protoTypes = {
+    data: PropTypes.object.isRequired,
+};
 export default AccountItem;
