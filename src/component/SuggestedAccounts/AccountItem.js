840bac5deb343ee23a2565a85e2a2283e3a05a97
HuynhQuyn <huynhquyn0811@example.com>
2023-11-01 21:20:26 +0700

    UI phần sidebar #3

--- src/component/SuggestedAccounts/AccountItem.js
+++ src/component/SuggestedAccounts/AccountItem.js
@@ -1,27 +1,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
+import AccountPreview from './AccountPreview';
+import { Wrapper as PopperWrapper } from '../Popper';
 import styles from './SuggestedAccounts.module.scss';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
-                alt=""
-            />
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>QuốcNguyễnVũ</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>Quốc Nguyễn Vũ</p>
+    const renderPreview = ({ props }) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
             </div>
+        );
+    };
+
+    return (
+        <div>
+            <Tippy offset={[-20, 0]} interactive delay={[800, 0]} render={renderPreview} placement="bottom">
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>QuốcNguyễnVũ</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quốc Nguyễn Vũ</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
